4b133770e92edb02122c1f943b548c4cc8a7465e
MelissaCFox <melissa.caitlin.fox@gmail.com>
2021-11-15 16:11:13 -0600

    new commit with repaired directory

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+export const KandyKorner = () => {
+    return (
+        <>
+        <h1>Kandy Korner</h1>
+        <h2>Locations</h2>
+        
+
+        </>
+
+    )
+}
